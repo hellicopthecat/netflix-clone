9050d7aadd1e51701611028cae291563f0015e7b
Hellicat <jhccop@naver.com>
2023-10-24 13:02:27 +0900

    first commit

---
+++ src/views/Home.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const HomeWrapper = styled.div`
+  width: 100%;
+  height: 200vh;
+  padding-top: 80px;
+`;
+export default function Home() {
+  return <HomeWrapper> this is Home</HomeWrapper>;
+}
